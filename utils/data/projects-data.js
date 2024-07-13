7e43d0669639bf52ed2daa50c517c0339d67ccdc
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2024-07-13 20:18:55 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -21,8 +21,18 @@ export const projectsData = [
     demo: "",
     image: crefin,
   },
-  {
+    {
     id: 2,
+    name: "React NPM package for handling listeners",
+    tools: ["React", "JavaScript", "Webpack", "Babel", "NPM"],
+    role: "Library Developer",
+    link: "https://www.npmjs.com/package/react-event-listener-utils",
+    demo: "test",
+    description:
+      "A custom React hook utility for managing event listeners on specified child elements based on a parent element or document directly. It provides flexible and efficient event handling by supporting dynamic callback functions, error handling, and optional attribute removal. This utility aims to simplify event management in React projects, making it easy to add, handle, and remove event listeners with minimal boilerplate code.",
+  },
+  {
+    id: 3,
     name: "My Portfolio Site",
     description:
       "This project is my personal portfolio site, built using Next.js for a seamless and efficient web experience. It leverages MongoDB to dynamically fetch and display data, ensuring the content is always up-to-date. The styling is powered by Tailwind CSS, providing a modern and responsive design. Email functionality is integrated using EmailJS, with added Google reCAPTCHA to ensure security against spam. Google Analytics is included to track visitor metrics and gain insights into user interactions",
