27dfae6ed8d3f62531f33c27a9309738a1aaf77b
p0t02gz <praveen.raj.thiagaraju@walmart.com>
2024-07-31 23:56:15 +0530

    added splash screen

--- app/components/homepage/splashscreen/index.jsx
+++ app/components/homepage/splashscreen/index.jsx
@@ -17,12 +17,12 @@ function SplashScreen() {
       <div id="temp-div" class="h-screen w-screen bg-black"></div>
       <div
         id="transition-container"
-        class="  bg-slate-900 transition-all duration-1000"
+        class="   bg-slate-900 transition-all duration-1000"
       >
         <div class="flex items-center justify-center h-full bg-black">
           <span
             id="fade-text"
-            class="text-transparent bg-clip-text bg-gradient-to-r from-[#662D8C] to-[#ED1E79] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold "
+            class=" text-center text-transparent bg-clip-text bg-gradient-to-r from-[#662D8C] to-[#ED1E79] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold "
           >
             WELCOME TO MY PORTFOLIO
           </span>
