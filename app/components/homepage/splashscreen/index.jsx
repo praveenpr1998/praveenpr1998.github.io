43613e462748d5c8c87ffb53fe92d8ca1e05e13d
p0t02gz <praveen.raj.thiagaraju@walmart.com>
2024-07-31 23:49:58 +0530

    added splash screen

--- app/components/homepage/splashscreen/index.jsx
+++ app/components/homepage/splashscreen/index.jsx
@@ -1,30 +1,34 @@
 "use client";
-import React from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 
 function SplashScreen() {
-  window.addEventListener("load", () => {
+  useLayoutEffect(() => {
     setTimeout(() => {
+      document.getElementById("temp-div").style.height = 0;
       document
         .getElementById("transition-container")
         .classList.add("animate-shrinkHeight");
       document.getElementById("fade-text").classList.add("animate-fadeOut");
     }, 0); // Delay before the transition starts
-  });
+  }, []);
 
   return (
-    <div
-      id="transition-container"
-      class="  bg-slate-900 transition-all duration-1000"
-    >
-      <div class="flex items-center justify-center h-full bg-black">
-        <span
-          id="fade-text"
-          class="text-transparent bg-clip-text bg-gradient-to-r from-[#662D8C] to-[#ED1E79] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold "
-        >
-          WELCOME TO MY PORTFOLIO
-        </span>
+    <>
+      <div id="temp-div" class="h-screen w-screen bg-black"></div>
+      <div
+        id="transition-container"
+        class="  bg-slate-900 transition-all duration-1000"
+      >
+        <div class="flex items-center justify-center h-full bg-black">
+          <span
+            id="fade-text"
+            class="text-transparent bg-clip-text bg-gradient-to-r from-[#662D8C] to-[#ED1E79] text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold "
+          >
+            WELCOME TO MY PORTFOLIO
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
