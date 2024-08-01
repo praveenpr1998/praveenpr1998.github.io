2c9017bcfb581d010c9f80ff6bccdc13420204aa
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2024-08-01 15:35:58 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SplashScreen />
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
