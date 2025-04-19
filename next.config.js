93538b97ef9ba3086901d6c02af2173049ccc30f
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2025-04-19 23:34:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,16 +7,21 @@ module.exports = {
   images: {
   domains: ["cdn-images-1.medium.com", "miro.medium.com", "res.cloudinary.com", "media.dev.to"],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "**",
-      },
-      {
-        protocol: "https",
-        hostname: "media.dev.to",
-        pathname: "**",
-      },
+       {
+      protocol: "https",
+      hostname: "miro.medium.com",
+      pathname: "**",
+    },
+    {
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      pathname: "**",
+    },
+    {
+      protocol: "https",
+      hostname: "media.dev.to",
+      pathname: "**",
+    },
     ],
   },
   experimental: {
